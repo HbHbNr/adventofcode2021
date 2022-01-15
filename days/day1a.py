927eb924c8723089cb6034c5937046550ddbb9e9
Sebastian <66569741+HbHbNr@users.noreply.github.com>
2022-01-15 02:31:45 +0100

    Solve day 1 a and b

---
+++ days/day1a.py
@@ -0,0 +1,16 @@
+import fileinput
+
+inputfile = "day1_input.txt"
+
+firstline = True
+lastvalue = 0
+increments = 0
+for line in fileinput.input(inputfile):
+    value = int(line)
+    if(firstline):
+        firstline = False
+    elif value > lastvalue:
+        increments += 1
+    lastvalue = value
+
+print(increments)
---
+++ days/day1b.py
@@ -0,0 +1,19 @@
+import fileinput
+
+inputfile = "day1_input.txt"
+
+lastwindow =[]
+increments = 0
+for line in fileinput.input(inputfile):
+    value = int(line)
+    if len(lastwindow)<3:
+        lastwindow.append(value)
+    else:
+        currentwindow = lastwindow[1:]
+        currentwindow.append(value)
+        if(sum(currentwindow) > sum(lastwindow)):
+            increments += 1
+        lastwindow = currentwindow
+    #print(lastwindow)
+
+print(increments)
