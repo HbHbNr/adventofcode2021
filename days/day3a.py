88c9496071b0eabc7ef7b8d6ddd026221b2d16ad
Sebastian <66569741+HbHbNr@users.noreply.github.com>
2022-01-17 23:10:28 +0100

    Improve day 3 a

--- days/day3a.py
+++ days/day3a.py
@@ -6,7 +6,7 @@ inputfile = "inputfiles/day3_input.txt"
 
 class Histogram:
     def __init__(self, maxindex):
-        self.frequencies = [[0, 0] for x in range(maxindex)]
+        self.frequencies = [{'0': 0, '1': 0} for x in range(maxindex)]
 
     def add(self, index, value):
         self.frequencies[index][value] += 1
@@ -15,24 +15,24 @@ class Histogram:
         parameter = []
         for index, values in enumerate(self.frequencies):
             # print(values)
-            diff = values[0] - values[1]
+            diff = values['0'] - values['1']
             diff = diff if mostcommon else -diff
             parameter.append('0' if diff > 0 else '1')
         return ''.join(parameter)
 
 
-def createhistogram():
+def createhistogram(inputfile):
     histogram = None
     for line in fileinput.input(inputfile):
         line = line.rstrip()
-        if fileinput.filelineno() == 1:
+        if fileinput.lineno() == 1:
             histogram = Histogram(len(line))
         for index, value in enumerate(line):
-            histogram.add(int(index), int(value))
+            histogram.add(index, value)
     return histogram
 
 
-histogram = createhistogram()
+histogram = createhistogram(inputfile)
 print(histogram.frequencies)
 gamma = histogram.buildparameter(True)
 epsilon = histogram.buildparameter(False)
