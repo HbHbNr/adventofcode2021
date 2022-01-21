54d5902a68c1e4f142ae5ddf0b8ebd6baf5919d5
Sebastian <66569741+HbHbNr@users.noreply.github.com>
2022-01-22 00:33:46 +0100

    Solve day 4 a and b combined, including tests

---
+++ days/day4both.py
@@ -0,0 +1,73 @@
+class BingoBoard:
+    def __init__(self, rows):
+        self._rows = [[int(x) for x in row.split(None)] for row in rows]
+        self._columns = [[] for x in range(5)]
+        for index, column in enumerate(self._columns):
+            for row in self._rows:
+                column.append(row[index])
+
+    def print(self, indent=''):
+        for row in self._rows:
+            print('{}{:2} {:2} {:2} {:2} {:2}'.format(indent, *row))
+
+    def playNumber(self, pickednumber):
+        for row_or_column in [*self._rows, *self._columns]:
+            if pickednumber in row_or_column:
+                # print(f'{pickednumber} found in {row_or_column}')
+                row_or_column[row_or_column.index(pickednumber)] = -1
+
+    def sum(self):
+        result = 0
+        for row in self._rows:
+            for number in row:
+                result += number if number != -1 else 0
+        return result
+
+    def isWinner(self):
+        result = self._rows.count([-1, -1, -1, -1, -1]) > 0 or self._columns.count([-1, -1, -1, -1, -1]) > 0
+        return result
+
+
+class BingoMatch:
+    def __init__(self, lines):
+        self._pickednumbers = [int(x) for x in lines[0].split(',')]
+        self._bingoboards = []
+        index = 2
+        while index < len(lines):
+            bingoboard = BingoBoard(lines[index:index+5])
+            self._bingoboards.append(bingoboard)
+            index += 6
+
+    def play(self):
+        pickednumbers = self._pickednumbers
+        bingoboards = self._bingoboards
+        winningbingoboards = []
+        for numberindex, pickednumber in enumerate(pickednumbers):
+            remainingbingoboards = []
+            for bingoboard in bingoboards:
+                # print(pickednumber)
+                # print(bingoboard.sum())
+                bingoboard.playNumber(pickednumber)
+                # print(bingoboard.sum())
+                if bingoboard.isWinner():
+                    boardindex = self._bingoboards.index(bingoboard)
+                    print(f'Winning board, numberindex {numberindex}, pickednumber {pickednumber}, boardindex {boardindex}:')
+                    bingoboard.print('  ')
+                    boardsum = bingoboard.sum()
+                    winningbingoboards.append({'numberindex': numberindex, 'pickednumber': pickednumber,
+                                               'boardindex': boardindex, 'boardsum': boardsum,
+                                               'solution': pickednumber * boardsum})
+                else:
+                    remainingbingoboards.append(bingoboard)
+            bingoboards = remainingbingoboards
+        return winningbingoboards
+
+
+if __name__ == '__main__':
+    import util
+
+    lines = util.readinputfile('inputfiles/day4_input.txt')
+    bingomatch = BingoMatch(lines)
+    winningbingoboards = bingomatch.play()
+    print('First board: ' + str(winningbingoboards[0]))
+    print(' Last board: ' + str(winningbingoboards[len(winningbingoboards)-1]))
