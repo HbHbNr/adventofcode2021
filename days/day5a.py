ff4683476e2ce27a469adea11f4bd711c579a442
Sebastian <66569741+HbHbNr@users.noreply.github.com>
2022-01-22 19:31:31 +0100

    Draw horizontal and vertical lines Fix print map

--- days/day5a.py
+++ days/day5a.py
@@ -1,3 +1,4 @@
+from math import copysign
 import re
 
 
@@ -9,21 +10,34 @@ class Map:
         ymax = 0
         for line in lines:
             match = self._linepattern.match(line)
-            # print(m.group(0))
-            # print(m.group('x1'))
-            # print(m.group('y1'))
-            # print(m.group('x2'))
-            # print(m.group('y2'))
+            # print(match.group(0))
+            # print(match.group('x1'))
+            # print(match.group('y1'))
+            # print(match.group('x2'))
+            # print(match.group('y2'))
             xmax = max(xmax, int(match.group('x1')), int(match.group('x2')))
             ymax = max(ymax, int(match.group('y1')), int(match.group('y2')))
         # print(f'xmax={xmax} ymax={ymax}')
         self._width = xmax + 1
         self._height = ymax + 1
         self._coords = [[0 for y in range(self._height)] for x in range(self._width)]
+        for line in lines:
+            match = self._linepattern.match(line)
+            # x1 = match.group('x1')
+            # y1 = match.group('y1')
+            # x2 = match.group('x2')
+            # y2 = match.group('y2')
+            self.drawline(int(match.group('x1')), int(match.group('y1')), int(match.group('x2')), int(match.group('y2')))
+
+    def drawline(self, x1, y1, x2, y2):
+        if x1 == x2 or y1 == y2:
+            for x in range(min(x1, x2), max(x1, x2) + 1):
+                for y in range(min(y1, y2), max(y1, y2) + 1):
+                    self._coords[x][y] += 1
 
     def print(self):
-        for x in range(self._width):
-            for y in range(self._height):
+        for y in range(self._height):
+            for x in range(self._width):
                 print(self._coords[x][y], end='')
             print()
 
