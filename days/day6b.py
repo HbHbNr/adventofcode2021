b88299cb8628a7750809bba032cc8fbbc0abe768
Sebastian <66569741+HbHbNr@users.noreply.github.com>
2022-01-23 12:44:54 +0100

    Solve day 6 b, including tests

---
+++ days/day6b.py
@@ -0,0 +1,37 @@
+class School:
+    def __init__(self, line):
+        self._fishstats = [0 for x in range(9)]
+        for fish in line.split(','):
+            self._fishstats[int(fish)] += 1
+
+    def __str__(self):
+        return str(self._fishstats)
+
+    def size(self):
+        return sum(self._fishstats)
+
+    def nextday(self):
+        creators = self._fishstats[0]
+        for index in range(len(self._fishstats) - 1):
+            self._fishstats[index] = self._fishstats[index + 1]
+        self._fishstats[6] += creators
+        self._fishstats[8] = creators
+
+    def nextdays(self, days, progress=False):
+        for day in range(1, days + 1):
+            if progress:
+                print(f'Day {day}')
+            self.nextday()
+
+
+if __name__ == '__main__':
+    import util
+
+    # lines = util.readinputfile('inputfiles/day6_example.txt')
+    lines = util.readinputfile('inputfiles/day6_input.txt')
+    school = School(lines[0])
+    print(school.size())
+    # print(school)
+    school.nextdays(256, False)
+    print(school.size())
+    # print(school)
---
+++ days/day6b_test.py
@@ -0,0 +1,25 @@
+from days import util, day6b
+
+
+def test_example():
+    lines = util.readinputfile('inputfiles/day6_example.txt')
+    school = day6b.School(lines[0])
+    assert school.size() == 5
+    school.nextday()
+    assert school.size() == 5
+    school.nextday()
+    assert school.size() == 6
+
+
+def test_example18():
+    lines = util.readinputfile('inputfiles/day6_example.txt')
+    school = day6b.School(lines[0])
+    school.nextdays(18)
+    assert school.size() == 26
+
+
+def test_example80():
+    lines = util.readinputfile('inputfiles/day6_example.txt')
+    school = day6b.School(lines[0])
+    school.nextdays(80)
+    assert school.size() == 5934
