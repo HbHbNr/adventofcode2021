d23972674064b1b31c05137b86ebdcf13862950b
Sebastian <66569741+HbHbNr@users.noreply.github.com>
2022-01-26 23:57:03 +0100

    Introduce class methods and static methods

--- days/day8a.py
+++ days/day8a.py
@@ -1,44 +1,42 @@
-# map number of chars to possible digit(s)
-# 2c -> 1
-# 3c -> 7
-# 4c -> 4
-# 5c -> 2, 3, 5
-# 6c -> 0, 6, 9
-# 7c -> 8
-
-# steps to determine all numbers
-# 7 - 1 -> a
-# 6c contains 4 -> 9, rest: 6c2
-# 6c2 contains 1 -> 0, rest: 6
-# 8 - 9 -> e
-# (8 - 0 -> d)
-# (8 - 6 -> c)
-# 6 - e -> 5  or without intermediate step:  6 - (8 - 9) -> 5
-# 5c remove 5 -> 5c2
-# 5c2 contains 1 -> 3, rest: 2
-
 class DigitMap:
-    def __init__(self):
-        self._mapnumberofcharstodigits = {2: [1], 3: [7], 4: [4], 5: [2, 3, 5], 6: [0, 6, 9], 7: [8]}
+    # map number of chars to possible digit(s)
+    # 2c -> 1
+    # 3c -> 7
+    # 4c -> 4
+    # 5c -> 2, 3, 5
+    # 6c -> 0, 6, 9
+    # 7c -> 8
+    _mapnumberofcharstodigits = {2: [1], 3: [7], 4: [4], 5: [2, 3, 5], 6: [0, 6, 9], 7: [8]}
 
-    def __getitem__(self, numberofchars):
-        return self._mapnumberofcharstodigits[numberofchars]
+    @classmethod
+    def numberofcharstodigits(cls, numberofchars):
+        return cls._mapnumberofcharstodigits[numberofchars]
+
+    @classmethod
+    def isdictinctnumberofchars(cls, numberofchars):
+        return len(cls.numberofcharstodigits(numberofchars)) == 1
 
 
 class Panel:
-    def __init__(self, digitmap, line):
-        self._digitmap = digitmap
-        self._parts = line.split(' ')
+    def __init__(self, line):
+        parts = line.split(' ')
+        self._patterns = parts[0:10]
+        self._outputs = parts[11:]
 
     def countdistinctdigits(self):
         count = 0
-        # print(self._parts)
-        for part in self._parts[11:]:
-            # print(part)
-            # print(len(part))
-            if len(self._digitmap[len(part)]) == 1:
+        for output in self._outputs:
+            if DigitMap.isdictinctnumberofchars(len(output)):
                 count += 1
         return count
+
+    @staticmethod
+    def countalldistinctdigits(lines):
+        totalcount = 0
+        for line in lines:
+            panel = Panel(line)
+            totalcount += panel.countdistinctdigits()
+        return totalcount
 
 
 if __name__ == '__main__':
@@ -46,9 +44,5 @@ if __name__ == '__main__':
 
     # lines = util.readinputfile('inputfiles/day8_example.txt')
     lines = util.readinputfile('inputfiles/day8_input.txt')
-    digitmap = DigitMap()
-    totalcount = 0
-    for line in lines:
-        panel = Panel(digitmap, line)
-        totalcount += panel.countdistinctdigits()
-    print(totalcount)
+    totalcount = Panel.countalldistinctdigits(lines)
+    print(f'totalcount: {totalcount}')
--- days/day8a_test.py
+++ days/day8a_test.py
@@ -3,13 +3,7 @@ from days import util, day8a
 
 def test_example():
     lines = util.readinputfile('inputfiles/day8_example.txt')
-    digitmap = day8a.DigitMap()
 
-    assert day8a.Panel(digitmap, lines[0]).countdistinctdigits() == 2
-    assert day8a.Panel(digitmap, lines[-1]).countdistinctdigits() == 2
-
-    totalcount = 0
-    for line in lines:
-        panel = day8a.Panel(digitmap, line)
-        totalcount += panel.countdistinctdigits()
-    assert totalcount == 26
+    assert day8a.Panel(lines[0]).countdistinctdigits() == 2
+    assert day8a.Panel(lines[-1]).countdistinctdigits() == 2
+    assert day8a.Panel.countalldistinctdigits(lines) == 26
