be3f12dd049b6096796cf7f2ff1133a413537039
HbHbNr <66569741+HbHbNr@users.noreply.github.com>
2023-06-02 00:18:52 +0200

    Solve day 11a

--- days/day11a.py
+++ days/day11a.py
@@ -8,31 +8,62 @@ class Cavern:
         self._maxx = len(lines[0])
         self._maxy = len(lines)
         self._rows = []
+        self._rows.append([0] * (1 + self._maxx + 1))
         for line in lines:
-            self._rows.append([int(energylevel) for energylevel in line])
+            row = [0] + [int(energylevel) for energylevel in line] + [0]
+            self._rows.append(row)
+        self._rows.append([0] * (1 + self._maxx + 1))
         self._totalFlashes = 0
-        return
 
     def step(self) -> None:
         self.incrementAll()
-        # while at least one 9 exists:
-        # - search for first 9
-        # - increase it to 10
-        # - increase adjacent by 1, but not over 9
-        # - repeat
-        # reset all 10s to 0
-        # reset frame to 0s
+
+        # while at least one >9 exists:
+        # - search for first >9
+        # - set it to 0 and increase totalFlashes
+        # - increase all adjacent by 1, if not already set to 0
+        oneflashed = True
+        while oneflashed:
+            oneflashed = False
+            for y in range(1, self._maxy + 1):
+                for x in range(1, self._maxx + 1):
+                    energyLevel = self.getEnergyLevel(x, y)
+                    if energyLevel > 9:
+                        self.flash(x, y)
+                        oneflashed = True
 
     def incrementAll(self) -> None:
-        for y in range(0, self._maxy):
-            for x in range(0, self._maxx):
-                self._rows[y][x] += 1
+        for y in range(1, self._maxy + 1):
+            for x in range(1, self._maxx + 1):
+                self.incrementEnergyLevel(x, y)
 
-    def totalFlashes(self) -> int:
+    def flash(self, x, y) -> None:
+        for yoffset in range(-1, 2):
+            for xoffset in range(-1, 2):
+                if yoffset == 0 and xoffset == 0:
+                    self.setEnergyLevel(x, y, 0)
+                    self._totalFlashes += 1
+                else:
+                    self.enlighten(x + xoffset, y + yoffset)
+
+    def enlighten(self, x, y) -> None:
+        energyLevel = self.getEnergyLevel(x, y)
+        if energyLevel == 0:
+            pass  # already flashed in this step or is from the frame
+        else:
+            self.setEnergyLevel(x, y, energyLevel + 1)
+
+    def getTotalFlashes(self) -> int:
         return self._totalFlashes
 
     def getEnergyLevel(self, x, y) -> int:
         return self._rows[y][x]
+
+    def setEnergyLevel(self, x, y, energyLevel: int) -> None:
+        self._rows[y][x] = energyLevel
+
+    def incrementEnergyLevel(self, x, y) -> None:
+        self._rows[y][x] += 1
 
     def __str__(self):
         lines = []
@@ -44,9 +75,12 @@ class Cavern:
 if __name__ == '__main__':
     from days import util
 
-    lines = util.readinputfile('inputfiles/day11_example.txt')
-    # lines = util.readinputfile('inputfiles/day11_input.txt')
+    # lines = util.readinputfile('inputfiles/day11_example.txt')
+    lines = util.readinputfile('inputfiles/day11_input.txt')
     cavern = Cavern(lines)
-    print(cavern)
-    cavern.step()
-    print(cavern)
+    # print(cavern)
+    # print(cavern.getTotalFlashes())
+    for _ in range(1, 101):
+        cavern.step()
+    # print(cavern)
+    print('DAY11A - Number of total flashes after 100 steps: ' + str(cavern.getTotalFlashes()))
--- days/day11a_test.py
+++ days/day11a_test.py
@@ -5,28 +5,40 @@ def test_example():
     lines = util.readinputfile('inputfiles/day11_example.txt')
     cavern = day11a.Cavern(lines)
 
-    assert cavern.totalFlashes() == 0
-    assert cavern.getEnergyLevel(0, 0) == 5  # top left
-    assert cavern.getEnergyLevel(-1, -1) == 6  # bottom right
+    # note: all coordinates must respect the additional frame of 0s
+
+    assert cavern.getTotalFlashes() == 0
+    assert cavern.getEnergyLevel(1, 1) == 5  # top left
+    assert cavern.getEnergyLevel(-2, -2) == 6  # bottom right
 
     cavern.step()  # step 1
-    assert cavern.totalFlashes() == 0
-    assert cavern.getEnergyLevel(0, 0) == 6  # top left
-    assert cavern.getEnergyLevel(-1, -1) == 7  # bottom right
+    assert cavern.getTotalFlashes() == 0
+    assert cavern.getEnergyLevel(1, 1) == 6  # top left
+    assert cavern.getEnergyLevel(-2, -2) == 7  # bottom right
 
     cavern.step()  # step 2
-    assert cavern.totalFlashes() == 35
-    assert cavern.getEnergyLevel(0, 0) == 8  # top left
-    assert cavern.getEnergyLevel(-1, -1) == 8  # bottom right
+    assert cavern.getTotalFlashes() == 35
+    assert cavern.getEnergyLevel(1, 1) == 8  # top left
+    assert cavern.getEnergyLevel(-2, -2) == 8  # bottom right
 
-    for step in range(3, 11):
+    for _ in range(3, 11):
         cavern.step()  # steps 3 to 10
-    assert cavern.totalFlashes() == 204
-    assert cavern.getEnergyLevel(0, 0) == 0  # top left
-    assert cavern.getEnergyLevel(-1, -1) == 0  # bottom right
+    assert cavern.getTotalFlashes() == 204
+    assert cavern.getEnergyLevel(1, 1) == 0  # top left
+    assert cavern.getEnergyLevel(-2, -2) == 0  # bottom right
 
-    for step in range(11, 101):
+    for _ in range(11, 101):
         cavern.step()  # steps 11 to 100
-    assert cavern.totalFlashes() == 1656
-    assert cavern.getEnergyLevel(0, 0) == 0  # top left
-    assert cavern.getEnergyLevel(-1, -1) == 6  # bottom right
+    assert cavern.getTotalFlashes() == 1656
+    assert cavern.getEnergyLevel(1, 1) == 0  # top left
+    assert cavern.getEnergyLevel(-2, -2) == 6  # bottom right
+
+def test_input():
+    lines = util.readinputfile('inputfiles/day11_input.txt')
+    cavern = day11a.Cavern(lines)
+
+    # note: all coordinates must respect the additional frame of 0s
+
+    for _ in range(1, 101):
+        cavern.step()  # steps 1 to 100
+    assert cavern.getTotalFlashes() == 1694
