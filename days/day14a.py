cf841de2dacb3055f662f8cea9ff04a5bfe2c662
HbHbNr <66569741+HbHbNr@users.noreply.github.com>
2023-06-12 00:36:51 +0200

    Solve day 14a

---
+++ days/day14a.py
@@ -0,0 +1,58 @@
+from typing import List, Tuple, Dict
+
+
+class Polymer:
+
+    def __init__(self, lines: List[str]) -> None:
+        self._template = lines[0]
+        self._rules: Dict[Tuple[str, str], str] = dict()
+
+        for line in lines[2:]:
+            key = line[0], line[1]
+            value = line[6]
+            self._rules[key] = value
+        # print(self._rules)
+
+    def step(self) -> None:
+        newtemplate: List[str] = []
+        for i in range(0, len(self._template) - 1):
+            a = self._template[i]
+            b = self._template[i + 1]
+            key = (a, b)
+            c = self._rules.get(key)
+            newtemplate.append(a)
+            if c is not None:
+                newtemplate.append(c)
+        newtemplate.append(self._template[-1])
+        self._template = ''.join(newtemplate)
+        # print(self._template)
+
+    def histogram(self) -> Dict[str, int]:
+        histogram: Dict[str, int] = dict()
+        for c in self._template:
+            if c in histogram:
+                histogram[c] += 1
+            else:
+                histogram[c] = 1
+        return histogram
+
+    def __str__(self):
+        return self._template
+
+
+if __name__ == '__main__':
+    from days import util
+
+    # lines = util.readinputfile('inputfiles/day14_example.txt')
+    lines = util.readinputfile('inputfiles/day14_input.txt')
+    polymer = Polymer(lines)
+    for step in range(1, 10 + 1):
+        polymer.step()
+        # print(f'{step} - {len(str(polymer))}')
+    histogram = polymer.histogram()
+    # print(histogram)
+    values = list(histogram.values())
+    values.sort()
+    diff = values[-1] - values[0]
+
+    util.printresultline('14a', diff)
---
+++ days/day14a_test.py
@@ -0,0 +1,54 @@
+from days import util, day14a
+
+
+def test_example1():
+    lines = util.readinputfile('inputfiles/day14_example.txt')
+    polymer = day14a.Polymer(lines)
+
+    assert str(polymer) == 'NNCB'
+
+    results = ['NCNBCHB',
+               'NBCCNBBBCBHCB',
+               'NBBBCNCCNBBNBNBBCHBHHBCHB',
+               'NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB']
+    for result in results:
+        polymer.step()
+        assert str(polymer) == result
+
+
+def test_example2():
+    lines = util.readinputfile('inputfiles/day14_example.txt')
+    polymer = day14a.Polymer(lines)
+
+    assert len(str(polymer)) == 4
+
+    for step in range(1, 5 + 1):
+        polymer.step()
+    assert len(str(polymer)) == 97
+
+    for step in range(6, 10 + 1):
+        polymer.step()
+    assert len(str(polymer)) == 3073
+
+
+def test_example3():
+    lines = util.readinputfile('inputfiles/day14_example.txt')
+    polymer = day14a.Polymer(lines)
+
+    for step in range(1, 10 + 1):
+        polymer.step()
+    histogram = polymer.histogram()
+    assert histogram.get('B') == 1749
+    assert histogram.get('C') == 298
+    assert histogram.get('H') == 161
+    assert histogram.get('N') == 865
+
+    values = list(histogram.values())
+    values.sort()
+    assert values[-1] - values[0] == 1588
+
+# def test_input():
+#     lines = util.readinputfile('inputfiles/day14_input.txt')
+#     polymer = day14a.Polymer(lines)
+#
+#     assert str(polymer) == '???'
