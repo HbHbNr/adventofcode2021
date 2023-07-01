7540d0db856bebad47c07ed3d910603bc6452e8e
HbHbNr <66569741+HbHbNr@users.noreply.github.com>
2023-07-01 02:23:56 +0200

    Add class ByteStream

--- days/day16a.py
+++ days/day16a.py
@@ -1,3 +1,22 @@
+from typing import Generator
+
+
+class ByteStream:
+
+    def __init__(self, bs: bytes) -> None:
+        self._bs = bs
+
+    @classmethod
+    def fromHexString(cls, hexString: str) -> 'ByteStream':
+        bs = bytes.fromhex(hexString)
+        byteStream = ByteStream(bs)
+        return byteStream
+
+    def getBytes(self) -> Generator:
+        for b in self._bs:
+            yield b
+
+
 if __name__ == '__main__':
     from days import util
 
@@ -5,3 +24,10 @@ if __name__ == '__main__':
     # util.readhexinputfile('inputfiles/day16_input.txt')
     for i in range(len(bs)):
         print(bs[i], bin(bs[i]))
+
+    byteStream = ByteStream.fromHexString('D2FE28')
+    getBytes = byteStream.getBytes()
+    b = next(getBytes, -1)
+    while b >= 0:
+        print(b)
+        b = next(getBytes, -1)
