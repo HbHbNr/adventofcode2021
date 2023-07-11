ed0615f20fab7cfec5d24e96f8a3788431a0721f
Sebastian <66569741+HbHbNr@users.noreply.github.com>
2023-07-12 00:30:47 +0200

    Add new support classes TokenStream, Token, and TokenType

---
+++ days/tokenstream.py
@@ -0,0 +1,54 @@
+from typing import Generator, NamedTuple, List
+from enum import Enum
+
+
+class TokenType(Enum):
+    SQUARE_BRACKET_OPEN = 1
+    SQUARE_BRACKET_CLOSE = 2
+    COMMA = 3
+    INTEGER = 4
+
+
+class Token(NamedTuple):
+
+    type: TokenType
+    intvalue: int
+
+    @classmethod
+    def createBasic(cls, s: str):
+        if s == '[':
+            type = TokenType.SQUARE_BRACKET_OPEN
+        elif s == ']':
+            type = TokenType.SQUARE_BRACKET_CLOSE
+        elif s == ',':
+            type = TokenType.COMMA
+        else:
+            raise ValueError(f'{s} has unknown TokenType')
+
+        return Token(type, 0)
+
+    @classmethod
+    def createInteger(cls, value: int):
+        return Token(TokenType.INTEGER, value)
+
+
+class TokenStream:
+
+    def __init__(self, s: str) -> None:
+        self._tokens: List[Token] = []
+        i = 0
+        while i < len(s):
+            if not str.isdecimal(s[i]):
+                self._tokens.append(Token.createBasic(s[i]))
+                i += 1
+            else:
+                value = 0
+                while i < len(s) and str.isdecimal(s[i]):
+                    value *= 10
+                    value += int(s[i])
+                    i += 1
+                self._tokens.append(Token.createInteger(value))
+
+    def stream(self) -> Generator:
+        for token in self._tokens:
+            yield token
---
+++ days/tokenstream_test.py
@@ -0,0 +1,40 @@
+from days import tokenstream
+
+
+def test_tokenstreamstring1():
+    stream = tokenstream.TokenStream('[[1,9],[18,105]]').stream()
+
+    assert next(stream) == tokenstream.Token(tokenstream.TokenType.SQUARE_BRACKET_OPEN, 0)
+    assert next(stream) == tokenstream.Token(tokenstream.TokenType.SQUARE_BRACKET_OPEN, 0)
+    assert next(stream) == tokenstream.Token(tokenstream.TokenType.INTEGER, 1)
+    assert next(stream) == tokenstream.Token(tokenstream.TokenType.COMMA, 0)
+    assert next(stream) == tokenstream.Token(tokenstream.TokenType.INTEGER, 9)
+    assert next(stream) == tokenstream.Token(tokenstream.TokenType.SQUARE_BRACKET_CLOSE, 0)
+    assert next(stream) == tokenstream.Token(tokenstream.TokenType.COMMA, 0)
+    assert next(stream) == tokenstream.Token(tokenstream.TokenType.SQUARE_BRACKET_OPEN, 0)
+    assert next(stream) == tokenstream.Token(tokenstream.TokenType.INTEGER, 18)
+    assert next(stream) == tokenstream.Token(tokenstream.TokenType.COMMA, 0)
+    assert next(stream) == tokenstream.Token(tokenstream.TokenType.INTEGER, 105)
+    assert next(stream) == tokenstream.Token(tokenstream.TokenType.SQUARE_BRACKET_CLOSE, 0)
+    assert next(stream) == tokenstream.Token(tokenstream.TokenType.SQUARE_BRACKET_CLOSE, 0)
+    try:
+        # buffer is empty now and raises StopIteration exception
+        next(stream)
+        assert False
+    except StopIteration:
+        assert True
+
+
+def test_tokenstreamstring2():
+    # test edge case where integer is end of string
+    stream = tokenstream.TokenStream('[[119').stream()
+
+    assert next(stream) == tokenstream.Token(tokenstream.TokenType.SQUARE_BRACKET_OPEN, 0)
+    assert next(stream) == tokenstream.Token(tokenstream.TokenType.SQUARE_BRACKET_OPEN, 0)
+    assert next(stream) == tokenstream.Token(tokenstream.TokenType.INTEGER, 119)
+    try:
+        # buffer is empty now and raises StopIteration exception
+        next(stream)
+        assert False
+    except StopIteration:
+        assert True
