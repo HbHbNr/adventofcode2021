348adb99e050b0625183fce31885ce1c5bae2e56
Sebastian <66569741+HbHbNr@users.noreply.github.com>
2023-07-12 17:53:29 +0200

    Add class TreeNode for day 18a

---
+++ days/day18a.py
@@ -0,0 +1,27 @@
+from typing import NamedTuple, Optional
+# from days import tokenstream
+
+
+class TreeNode(NamedTuple):
+
+    left: Optional['TreeNode']
+    right: Optional['TreeNode']
+    value: int
+
+    @classmethod
+    def createParent(cls, left: 'TreeNode', right: 'TreeNode') -> 'TreeNode':
+        return TreeNode(left, right, 0)
+
+    @classmethod
+    def createLeaf(cls, value: int) -> 'TreeNode':
+        return TreeNode(None, None, value)
+
+    def isLeaf(self):
+        return self.left is not None
+
+
+if __name__ == '__main__':
+    from days import util
+
+    # solved with https://github.com/HbHbNr/a-star
+    util.printresultline('18a', 0)
