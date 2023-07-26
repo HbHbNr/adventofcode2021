dbfc688307981111da6e522cb08f7d86c77e04fa
HbHbNr <66569741+HbHbNr@users.noreply.github.com>
2023-07-27 00:02:55 +0200

    Rewrite some tests to use unittest instead of pytest

--- util/bytestream_test.py
+++ util/bytestream_test.py
@@ -1,32 +1,33 @@
 """Unit tests for the ByteStream class"""
-import pytest
+import unittest
 from util import bytestream
 
 
-def testBytestreamstring():
-    stream = bytestream.ByteStream.fromHexString('D2FE28').stream()
+class TestByteStream(unittest.TestCase):
 
-    assert next(stream) == 0xd2
-    assert next(stream) == 0xfe
-    assert next(stream) == 0x28
-    with pytest.raises(StopIteration):
-        # buffer is empty now and raises StopIteration exception
-        next(stream)
+    def testByteStreamFromHexString(self):
+        stream = bytestream.ByteStream.fromHexString('D2FE28').stream()
 
+        assert next(stream) == 0xd2
+        assert next(stream) == 0xfe
+        assert next(stream) == 0x28
+        with self.assertRaises(StopIteration):
+            # buffer is empty now and raises StopIteration exception
+            next(stream)
 
-def testBytestreamfile():
-    # 8A004A801A8002F478
-    stream = bytestream.ByteStream.fromHexFile('inputfiles/day16_example.txt').stream()
+    def testByteStreamFromHexFile(self):
+        # 8A004A801A8002F478
+        stream = bytestream.ByteStream.fromHexFile('inputfiles/day16_example.txt').stream()
 
-    assert next(stream) == 0x8a
-    assert next(stream) == 0x00
-    assert next(stream) == 0x4a
-    assert next(stream) == 0x80
-    assert next(stream) == 0x1a
-    assert next(stream) == 0x80
-    assert next(stream) == 0x02
-    assert next(stream) == 0xf4
-    assert next(stream) == 0x78
-    with pytest.raises(StopIteration):
-        # buffer is empty now and raises StopIteration exception
-        next(stream)
+        assert next(stream) == 0x8a
+        assert next(stream) == 0x00
+        assert next(stream) == 0x4a
+        assert next(stream) == 0x80
+        assert next(stream) == 0x1a
+        assert next(stream) == 0x80
+        assert next(stream) == 0x02
+        assert next(stream) == 0xf4
+        assert next(stream) == 0x78
+        with self.assertRaises(StopIteration):
+            # buffer is empty now and raises StopIteration exception
+            next(stream)
--- util/tokenstream_test.py
+++ util/tokenstream_test.py
@@ -1,46 +1,45 @@
 """Unit tests for the Token and TokenStream classes"""
-import pytest
+import unittest
 from util import tokenstream
 
 
-def testTokenStreamNormal():
-    stream = tokenstream.TokenStream('[[1,9],[18,105]]').stream()
+class TestTokenStream(unittest.TestCase):
 
-    assert next(stream) == tokenstream.Token(tokenstream.TokenType.SQUARE_BRACKET_OPEN, 0)
-    assert next(stream) == tokenstream.Token(tokenstream.TokenType.SQUARE_BRACKET_OPEN, 0)
-    assert next(stream) == tokenstream.Token(tokenstream.TokenType.INTEGER, 1)
-    assert next(stream) == tokenstream.Token(tokenstream.TokenType.COMMA, 0)
-    assert next(stream) == tokenstream.Token(tokenstream.TokenType.INTEGER, 9)
-    assert next(stream) == tokenstream.Token(tokenstream.TokenType.SQUARE_BRACKET_CLOSE, 0)
-    assert next(stream) == tokenstream.Token(tokenstream.TokenType.COMMA, 0)
-    assert next(stream) == tokenstream.Token(tokenstream.TokenType.SQUARE_BRACKET_OPEN, 0)
-    assert next(stream) == tokenstream.Token(tokenstream.TokenType.INTEGER, 18)
-    assert next(stream) == tokenstream.Token(tokenstream.TokenType.COMMA, 0)
-    assert next(stream) == tokenstream.Token(tokenstream.TokenType.INTEGER, 105)
-    assert next(stream) == tokenstream.Token(tokenstream.TokenType.SQUARE_BRACKET_CLOSE, 0)
-    assert next(stream) == tokenstream.Token(tokenstream.TokenType.SQUARE_BRACKET_CLOSE, 0)
-    with pytest.raises(StopIteration):
-        # buffer is empty now and raises StopIteration exception
-        next(stream)
+    def testTokenStreamNormal(self):
+        stream = tokenstream.TokenStream('[[1,9],[18,105]]').stream()
 
+        assert next(stream) == tokenstream.Token(tokenstream.TokenType.SQUARE_BRACKET_OPEN, 0)
+        assert next(stream) == tokenstream.Token(tokenstream.TokenType.SQUARE_BRACKET_OPEN, 0)
+        assert next(stream) == tokenstream.Token(tokenstream.TokenType.INTEGER, 1)
+        assert next(stream) == tokenstream.Token(tokenstream.TokenType.COMMA, 0)
+        assert next(stream) == tokenstream.Token(tokenstream.TokenType.INTEGER, 9)
+        assert next(stream) == tokenstream.Token(tokenstream.TokenType.SQUARE_BRACKET_CLOSE, 0)
+        assert next(stream) == tokenstream.Token(tokenstream.TokenType.COMMA, 0)
+        assert next(stream) == tokenstream.Token(tokenstream.TokenType.SQUARE_BRACKET_OPEN, 0)
+        assert next(stream) == tokenstream.Token(tokenstream.TokenType.INTEGER, 18)
+        assert next(stream) == tokenstream.Token(tokenstream.TokenType.COMMA, 0)
+        assert next(stream) == tokenstream.Token(tokenstream.TokenType.INTEGER, 105)
+        assert next(stream) == tokenstream.Token(tokenstream.TokenType.SQUARE_BRACKET_CLOSE, 0)
+        assert next(stream) == tokenstream.Token(tokenstream.TokenType.SQUARE_BRACKET_CLOSE, 0)
+        with self.assertRaises(StopIteration):
+            # buffer is empty now and raises StopIteration exception
+            next(stream)
 
-def testTokenStreamIntegerAtEnd():
-    stream = tokenstream.TokenStream('[[119').stream()
+    def testTokenStreamIntegerAtEnd(self):
+        stream = tokenstream.TokenStream('[[119').stream()
 
-    assert next(stream) == tokenstream.Token(tokenstream.TokenType.SQUARE_BRACKET_OPEN, 0)
-    assert next(stream) == tokenstream.Token(tokenstream.TokenType.SQUARE_BRACKET_OPEN, 0)
-    assert next(stream) == tokenstream.Token(tokenstream.TokenType.INTEGER, 119)
-    with pytest.raises(StopIteration):
-        # buffer is empty now and raises StopIteration exception
-        next(stream)
+        assert next(stream) == tokenstream.Token(tokenstream.TokenType.SQUARE_BRACKET_OPEN, 0)
+        assert next(stream) == tokenstream.Token(tokenstream.TokenType.SQUARE_BRACKET_OPEN, 0)
+        assert next(stream) == tokenstream.Token(tokenstream.TokenType.INTEGER, 119)
+        with self.assertRaises(StopIteration):
+            # buffer is empty now and raises StopIteration exception
+            next(stream)
 
+    def testTokenStreamUnknownToken(self):
+        # test unknown token within the string
+        with self.assertRaises(ValueError):
+            tokenstream.TokenStream('[[1,9]#[18,105]]').stream()
 
-def testTokenStreamUnknownToken():
-    # test unknown token within the string
-    with pytest.raises(ValueError):
-        tokenstream.TokenStream('[[1,9]#[18,105]]').stream()
-
-
-def testTokenStr():
-    assert str(tokenstream.Token(tokenstream.TokenType.SQUARE_BRACKET_OPEN, 0)) == 'TokenType.SQUARE_BRACKET_OPEN'
-    assert str(tokenstream.Token(tokenstream.TokenType.INTEGER, 1)) == 'TokenType.INTEGER(1)'
+    def testTokenStr(self):
+        assert str(tokenstream.Token(tokenstream.TokenType.SQUARE_BRACKET_OPEN, 0)) == 'TokenType.SQUARE_BRACKET_OPEN'
+        assert str(tokenstream.Token(tokenstream.TokenType.INTEGER, 1)) == 'TokenType.INTEGER(1)'
