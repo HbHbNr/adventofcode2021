decf2f7db507477363f15bef07ea31c0621c02ee
HbHbNr <66569741+HbHbNr@users.noreply.github.com>
2023-08-07 02:46:35 +0200

    Solve day 18b

---
+++ days/day18b_test.py
@@ -0,0 +1,33 @@
+"""Unit tests for https://adventofcode.com/2021/day/18 part a"""
+import unittest
+from typing import List
+import itertools
+from days import day18a
+from util import tokenstream
+
+# typing.TypeAlias needs at least Python 3.10, so no type hints
+Token = tokenstream.Token
+TokenStream = tokenstream.TokenStream
+
+
+class TestDay18a(unittest.TestCase):
+
+    def testExample(self) -> None:
+        magnitudes: List[int] = []
+        tokenListList = day18a.MathHomework.readTokenFile('inputfiles/day18_example2.txt')
+        for pair in itertools.permutations(tokenListList, 2):
+            tokenList = day18a.MathHomework.addAndReduce(list(pair))
+            magnitude = day18a.MathHomework.calcMagnitude(token for token in tokenList)
+            magnitudes.append(magnitude)
+
+        assert max(magnitudes) == 3993
+
+    def testInput(self) -> None:
+        magnitudes: List[int] = []
+        tokenListList = day18a.MathHomework.readTokenFile('inputfiles/day18_input.txt')
+        for pair in itertools.permutations(tokenListList, 2):
+            tokenList = day18a.MathHomework.addAndReduce(list(pair))
+            magnitude = day18a.MathHomework.calcMagnitude(token for token in tokenList)
+            magnitudes.append(magnitude)
+
+        assert max(magnitudes) == 4555
