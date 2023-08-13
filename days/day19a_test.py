2908e725b30710adb3d0712e564355621d39061f
HbHbNr <66569741+HbHbNr@users.noreply.github.com>
2023-08-14 00:00:40 +0200

    Working on day 19a

--- days/day19a_test.py
+++ days/day19a_test.py
@@ -14,82 +14,82 @@ class TestDay19a(unittest.TestCase):
 
     def testVectorRotateAxes(self) -> None:
         coords = Coords(1, 2, 3)
-        assert str(coords) == '(1/2/3)'
+        assert str(coords) == 'C(1/2/3)'
         coords.rotateAxes()
-        assert str(coords) == '(3/1/2)'
+        assert str(coords) == 'C(3/1/2)'
         coords.rotateAxes()
-        assert str(coords) == '(2/3/1)'
+        assert str(coords) == 'C(2/3/1)'
         coords.rotateAxes()
-        assert str(coords) == '(1/2/3)'
+        assert str(coords) == 'C(1/2/3)'
 
     def testVectorTurnAroundX90(self) -> None:
         coords = Coords(1, 2, 3)
-        assert str(coords) == '(1/2/3)'
+        assert str(coords) == 'C(1/2/3)'
         coords.turnAroundX90()
-        assert str(coords) == '(1/-3/2)'
+        assert str(coords) == 'C(1/-3/2)'
         coords.turnAroundX90()
-        assert str(coords) == '(1/-2/-3)'
+        assert str(coords) == 'C(1/-2/-3)'
         coords.turnAroundX90()
-        assert str(coords) == '(1/3/-2)'
+        assert str(coords) == 'C(1/3/-2)'
         coords.turnAroundX90()
-        assert str(coords) == '(1/2/3)'
+        assert str(coords) == 'C(1/2/3)'
 
     def testVectorTurnAroundY90(self) -> None:
         coords = Coords(1, 2, 3)
-        assert str(coords) == '(1/2/3)'
+        assert str(coords) == 'C(1/2/3)'
         coords.turnAroundY90()
-        assert str(coords) == '(3/2/-1)'
+        assert str(coords) == 'C(3/2/-1)'
         coords.turnAroundY90()
-        assert str(coords) == '(-1/2/-3)'
+        assert str(coords) == 'C(-1/2/-3)'
         coords.turnAroundY90()
-        assert str(coords) == '(-3/2/1)'
+        assert str(coords) == 'C(-3/2/1)'
         coords.turnAroundY90()
-        assert str(coords) == '(1/2/3)'
+        assert str(coords) == 'C(1/2/3)'
 
     def testVectorTurnAroundZ90(self) -> None:
         coords = Coords(1, 2, 3)
-        assert str(coords) == '(1/2/3)'
+        assert str(coords) == 'C(1/2/3)'
         coords.turnAroundZ90()
-        assert str(coords) == '(-2/1/3)'
+        assert str(coords) == 'C(-2/1/3)'
         coords.turnAroundZ90()
-        assert str(coords) == '(-1/-2/3)'
+        assert str(coords) == 'C(-1/-2/3)'
         coords.turnAroundZ90()
-        assert str(coords) == '(2/-1/3)'
+        assert str(coords) == 'C(2/-1/3)'
         coords.turnAroundZ90()
-        assert str(coords) == '(1/2/3)'
+        assert str(coords) == 'C(1/2/3)'
 
     def testVectorTurnAroundX180(self) -> None:
         coords = Coords(1, 2, 3)
-        assert str(coords) == '(1/2/3)'
+        assert str(coords) == 'C(1/2/3)'
         coords.turnAroundX180()
-        assert str(coords) == '(1/-2/-3)'
+        assert str(coords) == 'C(1/-2/-3)'
         coords.turnAroundX180()
-        assert str(coords) == '(1/2/3)'
+        assert str(coords) == 'C(1/2/3)'
 
     def testVectorTurnAroundY180(self) -> None:
         coords = Coords(1, 2, 3)
-        assert str(coords) == '(1/2/3)'
+        assert str(coords) == 'C(1/2/3)'
         coords.turnAroundY180()
-        assert str(coords) == '(-1/2/-3)'
+        assert str(coords) == 'C(-1/2/-3)'
         coords.turnAroundY180()
-        assert str(coords) == '(1/2/3)'
+        assert str(coords) == 'C(1/2/3)'
 
     def testVectorTurnAroundZ180(self) -> None:
         coords = Coords(1, 2, 3)
-        assert str(coords) == '(1/2/3)'
+        assert str(coords) == 'C(1/2/3)'
         coords.turnAroundZ180()
-        assert str(coords) == '(-1/-2/3)'
+        assert str(coords) == 'C(-1/-2/3)'
         coords.turnAroundZ180()
-        assert str(coords) == '(1/2/3)'
+        assert str(coords) == 'C(1/2/3)'
 
     def testDistanceTurnLongestToPositiveX(self):
         distance = Distance(1, 2, -3)
-        assert str(distance) == '(1/2/-3)'
+        assert str(distance) == 'D(1/2/-3)'
         distance.turnLongestToPositiveX()
-        assert str(distance) == '(3/1/-2)'
+        assert str(distance) == 'D(3/-1/-2)'
 
     def testExample(self) -> None:
-        lines = util.readinputfile('inputfiles/day19_example.txt')
+        lines = util.readinputfile('inputfiles/day19_example2.txt')
         scannerData: ScannerData = day19a.ScannerData(lines)
 
         assert len(scannerData.getScanners()) == 5
