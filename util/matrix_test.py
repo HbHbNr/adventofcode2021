2fc2c72f32f0e1bbfd89a037ed954c4dc3155625
HbHbNr <66569741+HbHbNr@users.noreply.github.com>
2023-08-25 00:37:20 +0200

    Improve class Matrix and add tests

--- util/matrix_test.py
+++ util/matrix_test.py
@@ -1,13 +1,13 @@
 """Unit tests for the Matrix class"""
 import unittest
-import util
+from util import matrix
 
-Matrix3x3 = util.matrix.Matrix3x3
+Matrix3x3 = matrix.Matrix3x3
 
 
 class TestMatrix(unittest.TestCase):
 
     def testInit(self) -> None:
         identity = [1, 0, 0, 0, 1, 0, 0, 0, 1]
-        matrix = Matrix3x3(*identity)
-        assert str(matrix) == 'M(1/0/0 0/1/0 0/0/1)'
+        matrix3x3 = Matrix3x3(*identity)
+        assert str(matrix3x3) == 'M(1/0/0 0/1/0 0/0/1)'
